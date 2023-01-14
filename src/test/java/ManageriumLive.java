e3442b5bf12d92e77710b02b13accea5919316f5
etu296 <etumahmuda@gmail.com>
2023-01-14 22:37:25 +0600

    orangrHRM test

--- src/test/java/ManageriumLive.java
+++ src/test/java/ManageriumLive.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class ManageriumLive {
     WebDriver driver;
@@ -19,20 +20,34 @@ public class ManageriumLive {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.get("https://mgm.ibos.io/");
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
 
-    @Test
+    @Test()
     public  void logInLive()
     {
-        WebElement txtUser = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[4]/div[1]/input[1]"));
-        txtUser.sendKeys("01679172828");
-        WebElement txtPass = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[4]/div[2]/input[1]"));
-        txtPass.sendKeys("mgm@arp");
-        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
-        driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[4]/button[1]")).click();
-//        driver.findElement(By.xpath("//div[contains(text(),'HR')]")).click();
-//        driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[5]/ul[1]/li[1]/div[1]/div[1]/a[1]")).click();
-//        driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[1]/div[4]/form[1]/div[1]/div[1]/div[1]/div[2]/button[1]")).click();
+        WebElement txtUser = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[1]/div[1]/div[2]/input[1]"));
+        txtUser.sendKeys("Admin");
+        WebElement txtPass = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[2]/div[1]/div[2]/input[1]"));
+        txtPass.sendKeys("admin123");
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[3]/button[1]")).click();
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/button[1]")).click();
+        WebElement txtEmpName = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/input[1]"));
+        txtEmpName.sendKeys("Etuu");
+        WebElement txtlastname  = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]/div[2]/input[1]"));
+        txtlastname .sendKeys("Mahmuda1");
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[2]/div[1]/label[1]/span[1]")).click();
+        WebElement txtUsername = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+        txtUsername .sendKeys("etu259");
+        WebElement txtPass1 = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+        txtPass1.sendKeys("Etu12345@#");
+        WebElement txtPass2 = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[2]/input[1]"));
+        txtPass2.sendKeys("Etu12345@#");
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[2]/button[2]")).click();
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[2]/a[1]/span[1]")).click();
+        WebElement txtsearch = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/input[1]"));
+        txtsearch.sendKeys("Etu");
+        driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[2]/button[2]")).click();
     }
 }
